12bf450c6fd802fffc11b5b3a53bf3dcbd1f2221
1t1e1 <1t1e1@gmail.com>
2019-03-13 23:00:37 +0300

    En onemli kisimlari bitti.  ? Css resize kisima bakmam lazim.  0 Rest api dan kastedilen; tiklamayi post request olarak almam mi bilmiyorum

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require("path");
 var app = express();
 var mongoose = require('mongoose');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 mongoose.connect('mongodb://localhost:27017/Lamps', {useNewUrlParser: true});
@@ -17,13 +19,13 @@ var LampSchema = new Schema({
     required: true,
   }
 });
+
 // Create model of Lamp
 var Lamp = mongoose.model('Lamp', LampSchema);
 
 
 
 app.set('view engine', 'ejs');
-// app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.static(path.join(__dirname,'/public')))
  
 app.get('/', function(req, res) {
@@ -33,33 +35,39 @@ app.get('/', function(req, res) {
       return a.number - b.number
     })
     // this Lamps is array and contain all information 
-    res.render('pages/indexdb',{ arrayValued : Lamps});
+    res.render('pages/indexS',{ arrayValued : Lamps});
   })
 });
 
-// change page 
-app.get('/change/:id', function(req, res) {
-    let id = Number( req.params.id)
-    // console.log( typeof id)
-    // console.log( id)
-    Lamp.findOne({number:id},(err,Lamps)=>{
+
+io.on("connection", function(socket) {
+  socket.on('button clicked', (clicked_button)=>{   
+    var number = Number(clicked_button.id);
+    Lamp.findOne({number:number},(err,TheLamp)=>{
       if (err) return console.log("Something wrong when updating data!");
-      // console.log(Lamps.status)
-      Lamps.status = !Lamps.status
-      // console.log(Lamps.status)
-      // console.log(typeof Lamps)
-      // console.log(Lamps)
-      Lamps.save(function (err){
+      console.log(TheLamp);
+      TheLamp.status = !TheLamp.status
+      TheLamp.save(function (err){
         if (err) return console.log("Something wrong when saving data!")
       })
-      // .then((err) => console.log(typeof err));
-      res.redirect("/");
     })
+  
+  io.emit('button clicked', clicked_button);
+  })
+})
+ 
     
-    
-
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
 
-app.listen(3000, function(){
-  console.log('3000 is listening');
-});
+
+
+
+/* 
+  npm nin mongodb diye package'i varmis. ben mongoose kullandim.
+  ejs yerine baksa bir alternatifim var mi? 
+  Rest API tam olarak anlamadim. Tiklamayi post request olarak mi almam gerekiyor.
+    -> Public, klasor duzeni rest ile ilgili 
+  ? Css auto kismina biraz daha bakmam lazim  
+*/
